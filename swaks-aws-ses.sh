613a385073fc7175f33ddc8331ca8591a2685b78
davidpasek <david.pasek@gmail.com>
2019-05-19 21:03:35 +0200

    swaks email via AWS SES

---
+++ swaks-aws-ses.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Test SMTP server in AWS (SES - Simple Email Service)"
+swaks --to david.pasek@gmail.com \
+  --from=stan.zeleny@gmail.com \
+  --auth \
+  --auth-user=AKIAWJBJMHQSKYHX3RFS \
+  --auth-password \
+  --tls \
+  --server email-smtp.us-east-1.amazonaws.com \
+
